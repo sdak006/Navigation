4aeeb0122430bcb8053731303783f07225cf8351
unknown <sdak006@gmail.com>
2017-06-13 15:12:31 -0500

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { CreateStore } from 'redux';
 import firebase from 'firebase';
-import reducers from '/.reducers';
+import reducers from './reducers';
 
 class App extends Component {
     componentWillMount() {
-    const config = {
-        apiKey: 'AIzaSyAKSvW4pt8-0YHlGCxjARZmxm9GI8rbCDo',
-        authDomain: 'manager-e807e.firebaseapp.com',
-        databaseURL: 'https://manager-e807e.firebaseio.com',
-        projectId: 'manager-e807e',
-        storageBucket: 'manager-e807e.appspot.com',
-        messagingSenderId: '279815132743'
-    };
+        const config = {
+            apiKey: 'AIzaSyAKSvW4pt8-0YHlGCxjARZmxm9GI8rbCDo',
+            authDomain: 'manager-e807e.firebaseapp.com',
+            databaseURL: 'https://manager-e807e.firebaseio.com',
+            projectId: 'manager-e807e',
+            storageBucket: 'manager-e807e.appspot.com',
+            messagingSenderId: '279815132743'
+        };
 
     firebase.initializeApp(config);
     }
+
     render() {
         return (
             <Provider store={createStore(reducers)}>
